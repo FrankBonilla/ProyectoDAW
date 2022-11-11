7f02084bdca0930202894874c4232d6e94b4e243
frankbonilla <frankdeivis_17@hotmail.com>
2022-11-11 12:39:15 +0100

    #1 [Proyecto] Frontend: implementación de la barra de búsqueda en la tabla -> proyectos dados de baja.

--- Frontend/src/components/UnsuscribeProjects.vue
+++ Frontend/src/components/UnsuscribeProjects.vue
@@ -1,41 +1,65 @@
 <template>
     <v-main class="indigo lighten-5 fill-height pl-2 pr-2 pt-3">
-      <v-card  >
+      <v-card >
         <!--tabla de proyectos dados de baja -->
         <v-card-title class="justify-center blue-grey darken-3 white--text py-1">
             {{title}}
-        </v-card-title>
-      <v-simple-table class="text-left" >
-        <template v-slot:default>
-          
-          <thead class="blue-grey darken-3">
-            <tr>
-              <th class="white--text">Descripción</th>
-              <th class="white--text">Fecha de inicio</th>
-              <th class="white--text">Fecha fin</th>
-              <th class="white--text">Lugar</th>
-              <th class="white--text">Observaciones</th>
-              <th class="white--text">Fecha baja</th>
-              <th class="white--text">Acciones</th>
-            </tr>
-          </thead>
-          <tbody> <!--Recorremos el arreglo de proyectos -->
-            <tr v-for="proyect in projects" :key="proyect.id_proyecto">
-              <td>{{ proyect.descripcion }}</td>
-              <td>{{ proyect.fechaInicio | formatedDate }}</td>
-              <td v-if="proyect.fechaFin">{{ proyect.fechaFin | formatedDate }}</td>
-              <td v-else style="color:orange"> <b>No definida</b> </td>
-              <td :style="proyect.lugar? 'color: black' : 'color: orange;font-weight: bold' ">
-                {{ proyect.lugar? proyect.lugar : 'Sin asignar' }}</td>
-              <td>{{ proyect.observaciones }}</td>
-              <td>{{ proyect.fechaBaja | formatedDate }}</td>
-              <td>
-                <v-btn class="white--text" color="red" small @click="borrar(proyect)" title="eliminar"><b>X</b></v-btn>
-              </td>
-            </tr>
-          </tbody>
+      <v-spacer></v-spacer>
+        <v-text-field
+          v-model="search"
+          append-icon="mdi-magnify"
+          label="Buscar por nombre"
+          dark
+          single-line
+          hide-details
+        ></v-text-field>
+      </v-card-title>
+      <v-data-table
+                :headers="headers"
+                :items="projects"
+                :search="search"
+                :hide-default-footer="projects.length < 10 ? true : false"
+                :footer-props="{itemsPerPageText: 'Filas por página'}"
+                >
+         <template v-slot:[`item.fechaInicio`]="{item}">
+            {{ item.fechaInicio | formatedDate }}
+         </template>
+
+         <template v-slot:[`item.fechaFin`]="{item}">
+          <td v-if="item.fechaFin">
+              {{ item.fechaFin | formatedDate }}
+          </td>
+          <td v-else
+              style="color:orange ; font-weight: bold"
+          >No definida
+          </td>
+         </template>
+
+        <template v-slot:[`item.lugar`]="{item}">
+          <td v-if="item.lugar">
+              {{ item.lugar }}
+          </td>
+          <td v-else
+              style="color:orange ; font-weight: bold"
+          >Sin asignar
+          </td>
         </template>
-      </v-simple-table>
+        <template v-slot:[`item.fechaBaja`]="{item}">
+            {{ item.fechaBaja | formatedDate }}
+         </template>
+
+         <template v-slot:[`item.actions`]="{ item }">
+            <v-icon
+              medium
+              @click="borrar(item)"
+              title="eliminar"
+              color="red"
+              >
+              mdi-delete
+            </v-icon>
+        </template>
+      </v-data-table>
+
        </v-card>
     </v-main>
     
@@ -52,7 +76,16 @@ export default {
     data() {
         return {
             title: 'Proyectos dados de baja',
-            projects: []
+            projects: [],
+             //cabeceras de la tabla principal
+            search: '',
+            headers: [{text: 'Descripción', align: 'start', filtrable: false, value: 'descripcion', class:"blue-grey darken-3 ; white--text"},
+                      {text: 'Fecha Inicio', align: 'start', value: 'fechaInicio', class:"blue-grey darken-3 ; white--text"},
+                      {text: 'Fecha Fin', align: 'start', value: 'fechaFin', class:"blue-grey darken-3 ; white--text"},
+                      {text: 'Lugar', align: 'start', value: 'lugar', class:"blue-grey darken-3 ; white--text"},
+                      {text: 'Observaciones', align: 'start', value: 'observaciones', sortable: false, class:"blue-grey darken-3 ; white--text"},
+                      {text: 'Fecha Baja', align: 'start', value: 'fechaBaja', class:"blue-grey darken-3 ; white--text"},
+                      {text: 'Acciones', align: 'center', value: 'actions', sortable: false, class:"blue-grey darken-3 ; white--text"},],
         }
         
     },
